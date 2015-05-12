58215df55b932c8a4b22af41bb70258fecd4fc84
Randall <randallmeeker@gmail.com>
2015-05-12 15:21:04 -0500

    init

---
+++ Gulpfile.js
@@ -0,0 +1,24 @@
+
+var gulp = require('gulp'),
+	ts = require('gulp-typescript');
+
+var pubPath = ".tmp/public";
+
+var paths = {
+  ts : ['client/**/*.ts']
+};
+
+
+// TypeScript
+var tsProject = ts.createProject({
+   noExternalResolve: true,
+   target : 'ES5'
+});
+gulp.task('build-ts', function(){
+
+	var tsResult = gulp.src(paths.ts)
+		.pipe(ts(tsProject));
+
+    return tsResult.js.pipe(gulp.dest(pubPath));
+
+});
